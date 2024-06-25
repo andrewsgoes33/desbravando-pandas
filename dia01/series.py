c83b8ca011a94bf2f182a16255b245cad7313f03
Andrews Goes <andrews.goes.33@gmail.com>
2024-06-25 08:09:56 -0300

    Novas alterações estatisticas

--- dia01/series.py
+++ dia01/series.py
@@ -1,3 +1,29 @@
 # %%
 import pandas as pd
 # %%
+
+idades = [30, 42, 90, 34]
+idades
+# %%
+media = (sum(idades) / len(idades))
+
+total = 0
+
+for i in idades:
+    total += (media - i) **2
+    
+variancia = total / (len(idades) - 1)
+
+variancia
+# %%
+series_idades = pd.Series(idades)
+series_idades
+# %%
+series_idades.mean()
+# %%
+series_idades.var()
+# %%
+series_idades.describe()
+# %%
+series_idades.quantile(0.75)
+# %%
